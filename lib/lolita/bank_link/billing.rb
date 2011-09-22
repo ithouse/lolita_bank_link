b493cf434f9469306ac528713591ceeddeb7e7f2
Aivils Štoss <aivils@ithouse.lv>
2011-09-22 16:28:45 +0300

    bank_link_paid?

--- lib/lolita/bank_link/billing.rb
+++ lib/lolita/bank_link/billing.rb
@@ -4,11 +4,9 @@ module Lolita::BankLink
       base.has_many :bank_link_transactions, :as => :paymentable, :class_name => "Lolita::BankLink::Transaction", :dependent => :destroy
       base.class_eval do
 
-        def paid_with_bank_link?
+        def bank_link_paid?
           return true if self.bank_link_transactions.count(:conditions => {:status => 'completed'}) >= 1
-          paid_without_bank_link?
         end
-        alias_method_chain :paid?, :bank_link
 
         def log severity, message
           raise "Redefine this method in your billing model."
