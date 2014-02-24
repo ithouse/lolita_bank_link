1a0b48d17682416a95db07de8a2342147dad4641
Gatis Tomsons <gatis@ithouse.cc>
2014-02-24 10:29:30 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/checkout" => "lolita_bank_link/transactions#checkout", as: "checkout_bank_link"
-  get "/answer" => "lolita_bank_link/transactions#answer", as: "answer_bank_link"
-  get "/test/:action" => "lolita_bank_link/test"
+  get "/bank_link/checkout" => "lolita_bank_link/transactions#checkout", as: "checkout_bank_link"
+  get "/bank_link/answer" => "lolita_bank_link/transactions#answer", as: "answer_bank_link"
+  get "/bank_link/test/:action" => "lolita_bank_link/test"
 end
