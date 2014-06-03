678c0aeddf8c2ba76ad23fb7915a43e671802880
Gatis Tomsons <gatis@ithouse.cc>
2014-06-03 12:11:24 +0300

    fixed transaction ip decode

--- app/models/lolita_bank_link/transaction.rb
+++ app/models/lolita_bank_link/transaction.rb
@@ -10,7 +10,7 @@ module LolitaBankLink
     validates_associated :paymentable
 
     def ip
-      IPAddr.new(self[:ip], Socket::AF_INET).to_s
+      IPAddr.new(self[:ip].to_i, Socket::AF_INET).to_s
     end
 
     def ip=(x)
